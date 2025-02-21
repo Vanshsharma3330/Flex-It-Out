945ed81e3a8ee381c2a768dff4d6dee29d7f1183
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2025-02-21 15:21:27 +0530

    adding-yoga-session

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
-
+import Yoga from "./pages/Yoga";
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/yoga" element={<Yoga />} />
             </Routes>
         </BrowserRouter>
     );
