d8879180b60f4d77b666cb727e7e40058e5f35de
Varinder-Dhillon0 <98728916+Varinder-Dhillon0@users.noreply.github.com>
2025-02-21 14:36:01 +0530

    login and register routes created

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,25 @@ const app = express();
 require("dotenv").config({ path: "./.env" });
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const userRouter = require("./routes/user")
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.use(cors({
+    origin : process.env.ACCESS_URL,
+    credentials : true
+}))
+
 
 app.get("/", (req,res) =>{
 
-    console.log(process.env.ALLOWED_ORIGIN);
+    console.log(process.env.ACCESS_URL);
 
     res.send("hey there");
 })
+
+app.use(userRouter);
 
 
 app.listen(5000 , () =>{
